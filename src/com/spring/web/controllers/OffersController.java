b7ae3620dd8cff987ac2afac4e04c79f1c77b314
luke-PC\luke <kosmala.luke@gmail.com>
2017-03-29 10:52:41 +0200

    Initial web program

---
+++ src/com/spring/web/controllers/OffersController.java
@@ -0,0 +1,16 @@
+package com.spring.web.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+	
+	@RequestMapping("/")
+	public String showHome(HttpSession session) {
+		session.setAttribute("name", "Boris");
+		return "Home";
+	}
+}
